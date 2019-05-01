398bc3957c83e0b04dc8524cba8498727595dbf6
MichaelPaulHP <michael_h_p_@hotmail.com>
2019-04-30 22:36:25 -0500

    completado creo

--- app/src/main/java/com/example/mrrobot/concurrent/Config/MapBox.java
+++ app/src/main/java/com/example/mrrobot/concurrent/Config/MapBox.java
@@ -1,6 +1,6 @@
 package com.example.mrrobot.concurrent.Config;
 
 public class MapBox {
-    public static final String style="mapbox://styles/mrmichaelbot/cjl11jid42dus2smukx2nfmyh";
-
+    public static final String style="mapbox://styles/mrmichaelbot/cjtga713n6llo1fpirmvxmysj";
+    //public static final String style="mapbox://styles/mrmichaelbot/cjl11jid42dus2smukx2nfmyh";
 }
